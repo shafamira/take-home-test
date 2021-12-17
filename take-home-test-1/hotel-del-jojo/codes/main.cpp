03014c24a035044ecd547b27788e8943f9a9f309
Shafa Amira <shafaqonitatin@gmail.com>
2021-12-17 22:16:39 +0700

    edit main

--- take-home-test-1/hotel-del-jojo/codes/main.cpp
+++ take-home-test-1/hotel-del-jojo/codes/main.cpp
@@ -1,29 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-// #include <customer.h>
 #include "customer.h"
-
-// #define FILECUSTOMER "./customer/customer_data.txt";
 
 // prototypes
 void import_data_from_file();
 void menu();
 
 // struct
-struct Customer{
-    char id[100];
-    char name[100];
-    int age;
-    char reason[100];
-    char place[100];
-    int year[3000];
-    char idroom[100];
+struct Room{
+    char idroom[50];
     char room[100];
     int price;
 };
 
+struct Customer{
+    char id[50];
+    char name[100];
+    int age;
+    char reason[100];
+    char place[100];
+    int year;
+    Room r;
+};
+
 Customer customers[100];
+Room rooms[100];
+
+int idxCustomer = 0;
+int idxRoom = 0;
 
 // main
 int main(){
@@ -41,37 +46,19 @@ int main(){
            deleteData();
        }
     } while (choice == 5);
+    printf("Thank You!\n");
 
     return 0;
 }
 
 // menu
 void menu(){
-    // int choice;
     puts("Hotel del Jojo");
     puts("1. Add new customer\n2. Read data\n3. Update data");
     puts("4. Remove data\n5. Exit");
     printf("\nChoice [1-5] >> ");
 }
 
-// void import_data_from_file(){
-//     FILE *fcus;
-//     struct Customer c;
-//     if(fcus == fopen("FILECUSTOMER", "r") == NULL) printf("Error: Can't open file\n");
-//     else{
-//         while(fscanf(fcus,"%s,%s,%d,%[^,],%[^,],%d\n", c.id, c.name, c.age, c.reason, c.place, c.year, c.idroom)!= EOF){
-//             printf("|   | ID   | Name  | Age | Room   |\n");
-//             printf("|---|------|-------|-----|--------|\n");
-            
-//         }
-//     }
-// }
-
-
-// // // read
-// void readData(){
-//     struct Customer cus;
-//     printf("|   | ID   | Name  | Age | Room   |\n");
-//     printf("|---|------|-------|-----|--------|\n");
-//     printf("|   | COO1 | Dodi  | 18  | Pantai |\n");
-// }
+void import_data_from_file(){
+    
+}
